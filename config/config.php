46d43d5aba60c35d78f125b1e6a49cee25086ffc
Asahi5000 <joobias29@gmail.com>
2025-10-23 17:19:50 +0800

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,9 +1,9 @@
 <?php
-$host = 'db'; // internal Wasmer hostname
-$port = 3306;
+$host = 'db.fr-par1.bengt.wasmernet.com';
+$port = 10272;
 $dbname = 'car_ecommerce';
 $username = '9ef7aec87d4a8000ccdb856df051';
-$password = '068f9ef7-aec8-7f33-8000-1e0981ccd069'; // your actual DB password
+$password = '068f9ef7-aec8-7f33-8000-1e0981ccd069';
 
 try {
     $pdo = new PDO("mysql:host=$host;port=$port;dbname=$dbname;charset=utf8", $username, $password, [
