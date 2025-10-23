a8088274e62831a1d8d136159bcc3e963df8024c
Asahi5000 <joobias29@gmail.com>
2025-10-23 18:19:53 +0800

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -11,7 +11,7 @@ unset($_SESSION['error']);
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width,initial-scale=1.0">
-    <title>Admnin Login Page</title>
+    <title>Admin Login Page</title>
     <link rel="stylesheet" href="../admin/assets/css/login-styles.css">
     <link rel="stylesheet" href="../admin/assets/css/index-modal.css">
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
