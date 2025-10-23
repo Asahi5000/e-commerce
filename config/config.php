79ae1d507548c889c80c40e9f25a003c29a4d9d4
Asahi5000 <joobias29@gmail.com>
2025-10-23 17:17:11 +0800

    Change database password in config.php
    Updated database password in config file.

--- config/config.php
+++ config/config.php
@@ -3,7 +3,7 @@ $host = 'db.fr-par1.bengt.wasmernet.com';
 $port = 10272;
 $dbname = 'car_ecommerce';
 $username = '9ef7aec87d4a8000ccdb856df051';
-$password = 'YOUR_DB_PASSWORD_HERE'; // Replace this with the actual password
+$password = '068f9ef7-aec8-7f33-8000-1e0981ccd069'; // Replace this with the actual password
 
 try {
     $pdo = new PDO("mysql:host=$host;port=$port;dbname=$dbname;charset=utf8", $username, $password, [
